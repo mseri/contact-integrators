12c690932b278fcf3be757e59d62dd3362ad1552
Marcello Seri <marcello.seri@gmail.com>
2019-08-14 00:44:55 +0200

    Move pad_and_cumsum to common
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>

--- integrators/common.py
+++ integrators/common.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pylab as plb
 
+
 def rk4(system, init, tspan):
     """
     RungeKutta4 from matplotlib.pylab for the damped oscillator
@@ -12,3 +13,22 @@ def rk4(system, init, tspan):
         dp = -system.Vq(x[len(x)//2:], t) - system.f(t)*x[:len(x)//2]
         return tuple(np.concatenate([dp, dq]))
     return plb.rk4(derivs, init, tspan)
+
+
+# From https://stackoverflow.com/questions/30399534/shift-elements-in-a-numpy-array
+def shift(arr, num, fill_value=np.nan):
+    result = np.empty_like(arr)
+    if num > 0:
+        result[:num] = fill_value
+        result[num:] = arr[:-num]
+    elif num < 0:
+        result[num:] = fill_value
+        result[:num] = arr[-num:]
+    else:
+        result[:] = arr
+    return result
+
+
+def pad_and_cumsum(vec):
+    cumsum = np.cumsum(vec)
+    return shift(cumsum, 1, 0.0)
